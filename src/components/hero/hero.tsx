b3a31da9197d1c2f8a26b6ee03bb3df5796387b8
samuelribeiroo <samuelribeirodev@gmail.com>
2024-11-26 16:11:17 -0300

    update:appropriate escape sequence

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -110,7 +110,7 @@ export default function HeroSection() {
 						</h2>
 						<p className="text-muted-foreground mt-6">
 							Lorem Ipsum is simply dummy text of the printing and typesetting
-							industry. Lorem Ipsum has been the industry's standard dummy text
+							industry. Lorem Ipsum has been the industry&apos;s standard dummy text
 							ever since the 1500s, when an unknown printer took a galley of
 							type and scrambled it to make a type specimen book.
 						</p>
